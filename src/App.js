9beccd1966776e0e654d76f419c93358a5c70150
Amanda Hogan <ahogan@teamworks.com>
2020-01-27 10:43:59 -0500

    Add links to navigation component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
